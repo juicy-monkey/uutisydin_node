5df6e2f8046b86612e8bfdca8dfb5bb72d12523f
Jusa Mankki <jusamankki@gmail.com>
2025-04-02 18:02:24 +0300

    create image data site

---
+++ public/script.js
@@ -0,0 +1,485 @@
+const images = {
+  "alexander stubb": [
+    {
+      "filename": "1.png",
+      "source": "www.example.com",
+      "license": "CC-BY"
+    },
+    {
+      "filename": "2.jpg",
+      "source": "www.example.com",
+      "license": "CC-BY"
+    },
+    {
+      "filename": "3.jpg",
+      "source": "www.example.com",
+      "license": "CC-BY"
+    },
+    {
+      "filename": "4.jpg",
+      "source": "www.example.com",
+      "license": "CC-BY"
+    }
+  ],
+  "asuminen": [
+    {
+      "filename": "1.jpg",
+      "source": "www.example.com",
+      "license": "CC-BY"
+    },
+    {
+      "filename": "2.jpg",
+      "source": "www.example.com",
+      "license": "CC-BY"
+    }
+  ],
+  "aurinkovoima": [
+    {
+      "filename": "1.jpeg",
+      "source": "www.example.com",
+      "license": "CC-BY"
+    },
+    {
+      "filename": "2.jpeg",
+      "source": "www.example.com",
+      "license": "CC-BY"
+    }
+  ],
+  "benjamin netanjahu": [
+    {
+      "filename": "1.jpeg",
+      "source": "www.example.com",
+      "license": "CC-BY"
+    },
+    {
+      "filename": "2.jpeg",
+      "source": "www.example.com",
+      "license": "CC-BY"
+    }
+  ],
+  "donald trump": [
+    {
+      "filename": "1.jpg",
+      "source": "www.example.com",
+      "license": "CC-BY"
+    },
+    {
+      "filename": "2.png",
+      "source": "www.example.com",
+      "license": "CC-BY"
+    },
+    {
+      "filename": "3.jpg",
+      "source": "www.example.com",
+      "license": "CC-BY"
+    }
+  ],
+  "eduskunta": [
+    {
+      "filename": "1.jpeg",
+      "source": "www.example.com",
+      "license": "CC-BY"
+    },
+    {
+      "filename": "2.jpg",
+      "source": "www.example.com",
+      "license": "CC-BY"
+    },
+    {
+      "filename": "3.jpg",
+      "source": "www.example.com",
+      "license": "CC-BY"
+    },
+    {
+      "filename": "4.jpg",
+      "source": "www.example.com",
+      "license": "CC-BY"
+    }
+  ],
+  "emmanuel macron": [
+    {
+      "filename": "1.jpeg",
+      "source": "www.example.com",
+      "license": "CC-BY"
+    },
+    {
+      "filename": "2.jpeg",
+      "source": "www.example.com",
+      "license": "CC-BY"
+    },
+    {
+      "filename": "3.jpg",
+      "source": "www.example.com",
+      "license": "CC-BY"
+    }
+  ],
+  "euroopan keskuspankki": [
+    {
+      "filename": "1.jpg",
+      "source": "www.example.com",
+      "license": "CC-BY"
+    },
+    {
+      "filename": "2.jpeg",
+      "source": "www.example.com",
+      "license": "CC-BY"
+    }
+  ],
+  "euroopan unioni": [
+    {
+      "filename": "1.jpg",
+      "source": "www.example.com",
+      "license": "CC-BY"
+    },
+    {
+      "filename": "2.jpg",
+      "source": "www.example.com",
+      "license": "CC-BY"
+    }
+  ],
+  "eurooppa": [
+    {
+      "filename": "1.png",
+      "source": "www.example.com",
+      "license": "CC-BY"
+    }
+  ],
+  "helsinki": [
+    {
+      "filename": "1.jpg",
+      "source": "www.example.com",
+      "license": "CC-BY"
+    }
+  ],
+  "hsl": [
+    {
+      "filename": "1.jpg",
+      "source": "www.example.com",
+      "license": "CC-BY"
+    }
+  ],
+  "itämeri": [
+    {
+      "filename": "1.jpg",
+      "source": "www.example.com",
+      "license": "CC-BY"
+    }
+  ],
+  "jussi halla-aho": [
+    {
+      "filename": "1.jpg",
+      "source": "www.example.com",
+      "license": "CC-BY"
+    },
+    {
+      "filename": "2.jpeg",
+      "source": "www.example.com",
+      "license": "CC-BY"
+    }
+  ],
+  "leipomo": [
+    {
+      "filename": "1.jpeg",
+      "source": "www.example.com",
+      "license": "CC-BY"
+    }
+  ],
+  "mark rutte": [
+    {
+      "filename": "1.jpg",
+      "source": "www.example.com",
+      "license": "CC-BY"
+    }
+  ],
+  "metsä": [
+    {
+      "filename": "1.jpg",
+      "source": "www.example.com",
+      "license": "CC-BY"
+    }
+  ],
+  "nato": [
+    {
+      "filename": "1.jpg",
+      "source": "www.example.com",
+      "license": "CC-BY"
+    },
+    {
+      "filename": "2.jpg",
+      "source": "www.example.com",
+      "license": "CC-BY"
+    }
+  ],
+  "oikeudenkäynti": [
+    {
+      "filename": "1.jpeg",
+      "source": "www.example.com",
+      "license": "CC-BY"
+    },
+    {
+      "filename": "2.jpeg",
+      "source": "www.example.com",
+      "license": "CC-BY"
+    }
+  ],
+  "olaf scholz": [
+    {
+      "filename": "1.jpg",
+      "source": "www.example.com",
+      "license": "CC-BY"
+    },
+    {
+      "filename": "2.jpg",
+      "source": "www.example.com",
+      "license": "CC-BY"
+    }
+  ],
+  "osakemarkkinat": [
+    {
+      "filename": "1.jpeg",
+      "source": "www.example.com",
+      "license": "CC-BY"
+    },
+    {
+      "filename": "2.jpeg",
+      "source": "www.example.com",
+      "license": "CC-BY"
+    }
+  ],
+  "pentagon": [
+    {
+      "filename": "1.jpg",
+      "source": "www.example.com",
+      "license": "CC-BY"
+    }
+  ],
+  "petteri orpo": [
+    {
+      "filename": "1.jpeg",
+      "source": "www.example.com",
+      "license": "CC-BY"
+    },
+    {
+      "filename": "2.jpeg",
+      "source": "www.example.com",
+      "license": "CC-BY"
+    }
+  ],
+  "ranska": [
+    {
+      "filename": "1.jpg",
+      "source": "www.example.com",
+      "license": "CC-BY"
+    },
+    {
+      "filename": "2.jpg",
+      "source": "www.example.com",
+      "license": "CC-BY"
+    }
+  ],
+  "riikka purra": [
+    {
+      "filename": "1.jpeg",
+      "source": "www.example.com",
+      "license": "CC-BY"
+    }
+  ],
+  "saksa": [
+    {
+      "filename": "1.png",
+      "source": "www.example.com",
+      "license": "CC-BY"
+    },
+    {
+      "filename": "2.jpg",
+      "source": "www.example.com",
+      "license": "CC-BY"
+    }
+  ],
+  "suomen hallitus": [
+    {
+      "filename": "1.jpeg",
+      "source": "www.example.com",
+      "license": "CC-BY"
+    }
+  ],
+  "sähkö": [
+    {
+      "filename": "1.jpg",
+      "source": "www.example.com",
+      "license": "CC-BY"
+    },
+    {
+      "filename": "2.jpg",
+      "source": "www.example.com",
+      "license": "CC-BY"
+    },
+    {
+      "filename": "3.jpg",
+      "source": "www.example.com",
+      "license": "CC-BY"
+    },
+    {
+      "filename": "4.jpg",
+      "source": "www.example.com",
+      "license": "CC-BY"
+    }
+  ],
+  "talous": [
+    {
+      "filename": "1.jpeg",
+      "source": "www.example.com",
+      "license": "CC-BY"
+    },
+    {
+      "filename": "2.jpeg",
+      "source": "www.example.com",
+      "license": "CC-BY"
+    },
+    {
+      "filename": "3.jpg",
+      "source": "www.example.com",
+      "license": "CC-BY"
+    },
+    {
+      "filename": "4.jpg",
+      "source": "www.example.com",
+      "license": "CC-BY"
+    }
+  ],
+  "terveydenhuolto": [
+    {
+      "filename": "1.png",
+      "source": "www.example.com",
+      "license": "CC-BY"
+    },
+    {
+      "filename": "3.jpg",
+      "source": "www.example.com",
+      "license": "CC-BY"
+    },
+    {
+      "filename": "4.jpg",
+      "source": "www.example.com",
+      "license": "CC-BY"
+    }
+  ],
+  "tuulivoima": [
+    {
+      "filename": "1.jpg",
+      "source": "www.example.com",
+      "license": "CC-BY"
+    }
+  ],
+  "ukraina": [
+    {
+      "filename": "1.jpg",
+      "source": "www.example.com",
+      "license": "CC-BY"
+    }
+  ],
+  "venäjä": [
+    {
+      "filename": "1.jpeg",
+      "source": "www.example.com",
+      "license": "CC-BY"
+    }
+  ],
+  "vladimir putin": [
+    {
+      "filename": "1.jpeg",
+      "source": "www.example.com",
+      "license": "CC-BY"
+    }
+  ],
+  "volodymyr zelenskyi": [
+    {
+      "filename": "1.jpeg",
+      "source": "www.example.com",
+      "license": "CC-BY"
+    },
+    {
+      "filename": "2.jpeg",
+      "source": "www.example.com",
+      "license": "CC-BY"
+    },
+    {
+      "filename": "3.jpg",
+      "source": "www.example.com",
+      "license": "CC-BY"
+    }
+  ],
+  "yhdysvallat": [
+    {
+      "filename": "1.jpeg",
+      "source": "www.example.com",
+      "license": "CC-BY"
+    },
+    {
+      "filename": "2.jpeg",
+      "source": "www.example.com",
+      "license": "CC-BY"
+    },
+    {
+      "filename": "3.jpeg",
+      "source": "www.example.com",
+      "license": "CC-BY"
+    }
+  ]
+};
+
+
+
+
+
+const baseImageURL = "https://juicy-monkey.github.io/uutisydin_node";
+// const baseImageURL = "file:///Users/jusa/workspace/Juicy_Monkey/projects/uutisydin/uutisydin_node/public";
+
+function displayImages(imageData) {
+  const container = document.getElementById("image-list");
+  
+  for (const keyword in imageData) {
+    // Category title
+    const categoryTitle = document.createElement("h2");
+    categoryTitle.textContent = keyword;
+    container.appendChild(categoryTitle);
+    
+    const list = document.createElement("div");
+    list.className = "list";
+    
+    imageData[keyword].forEach(image => {
+      const source = `${baseImageURL}/images/${keyword}/${image.filename}`;
+      console.log(source)
+
+      const imgDiv = document.createElement("div");
+      imgDiv.className = "img-div";
+
+      const img = document.createElement("img");
+      img.src = source;
+      img.alt = `${keyword}/${image.filename}`;
+      img.loading = "lazy";
+
+      const link = document.createElement("a");
+      link.href = image.source;
+      link.textContent = image.source;
+      link.target = "_blank";
+      link.rel = "noopener noreferrer";
+
+      const license = document.createElement("p");
+      license.textContent = `License: ${image.license}`;
+
+      imgDiv.appendChild(img);
+      imgDiv.appendChild(link);
+      imgDiv.appendChild(license);
+      list.appendChild(imgDiv);
+    });
+
+    container.appendChild(list);
+  }
+}
+
+// Run when DOM is loaded
+document.addEventListener("DOMContentLoaded", () => {
+  displayImages(images);
+});
+
