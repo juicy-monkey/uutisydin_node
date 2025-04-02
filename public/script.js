6abff9dab2b2bf8afcac2c3115959e5af2bd837d
Jusa Mankki <jusamankki@gmail.com>
2025-04-02 19:32:28 +0300

    all licences

--- public/script.js
+++ public/script.js
@@ -7,433 +7,417 @@ const images = {
     },
     {
       "filename": "2.jpg",
-      "source": "www.example.com",
-      "license": "CC-BY"
+      "source": "https://www.flickr.com/photos/nato/53775161166/",
+      "license": "CC BY-NC-ND 2.0"
     },
     {
       "filename": "3.jpg",
-      "source": "www.example.com",
-      "license": "CC-BY"
+      "source": "https://www.flickr.com/photos/friendsofeurope/44555692744",
+      "license": "CC BY-NC-ND 2.0"
     },
     {
       "filename": "4.jpg",
-      "source": "www.example.com",
-      "license": "CC-BY"
+      "source": "https://www.flickr.com/photos/arcticfrontiers/16299086946/",
+      "license": "CC BY-NC 2.0"
     }
   ],
   "asuminen": [
     {
       "filename": "1.jpg",
-      "source": "www.example.com",
-      "license": "CC-BY"
+      "source": "https://northeastsecuritysolutions.com/what-makes-locked-doors-vulnerable/",
+      "license": "CC0 1.0"
     },
     {
       "filename": "2.jpg",
-      "source": "www.example.com",
-      "license": "CC-BY"
+      "source": "https://www.flickr.com/photos/26344495@N05/51018872701",
+      "license": "CC BY 2.0"
     }
   ],
   "aurinkovoima": [
     {
-      "filename": "1.jpeg",
-      "source": "www.example.com",
-      "license": "CC-BY"
+      "filename": "1.jpg",
+      "source": "https://www.flickr.com/photos/oregondot/3049032681",
+      "license": "CC BY 2.0"
     },
     {
-      "filename": "2.jpeg",
-      "source": "www.example.com",
-      "license": "CC-BY"
+      "filename": "2.jpg",
+      "source": "https://www.flickr.com/photos/151836356@N08/34514872834",
+      "license": "CC BY 2.0"
     }
   ],
   "benjamin netanjahu": [
     {
       "filename": "1.jpeg",
-      "source": "www.example.com",
-      "license": "CC-BY"
+      "source": "https://blogs.timesofisrael.com/netanyahu-and-israels-inflection-point/",
+      "license": "CC0 1.0"
     },
     {
       "filename": "2.jpeg",
-      "source": "www.example.com",
-      "license": "CC-BY"
+      "source": "https://www.heute.at/i/netanjahu-6-geiseln-kommen-noch-diese-woche-frei-120086991/doc-1iij7jo5k4",
+      "license": "CC BY 4.0"
     }
   ],
   "donald trump": [
     {
       "filename": "1.jpg",
-      "source": "www.example.com",
-      "license": "CC-BY"
+      "source": "https://snl.no/Donald_Trump",
+      "license": "CC BY-SA 2.0"
     },
     {
       "filename": "2.png",
-      "source": "www.example.com",
-      "license": "CC-BY"
+      "source": "https://www.opinium.com/resource-center/tump-in-a-uk-context/",
+      "license": "CC0 1.0"
     },
     {
       "filename": "3.jpg",
-      "source": "www.example.com",
-      "license": "CC-BY"
+      "source": "https://medillonthehill.medill.northwestern.edu/2024/11/market/",
+      "license": "CC0 1.0"
     }
   ],
   "eduskunta": [
     {
       "filename": "1.jpeg",
-      "source": "www.example.com",
-      "license": "CC-BY"
+      "source": "https://commons.wikimedia.org/wiki/File:Eduskunta_-_Parliament_House,_Helsinki_%2852890871089%29.jpg",
+      "license": "CC BY-SA 2.0"
     },
     {
       "filename": "2.jpg",
-      "source": "www.example.com",
-      "license": "CC-BY"
+      "source": "https://jenikirbyhistory.getarchive.net/media/eduskuntatalo-helsinki-cc9d3f",
+      "license": "PDM 1.0"
     },
     {
       "filename": "3.jpg",
-      "source": "www.example.com",
-      "license": "CC-BY"
+      "source": "https://snl.no/Eduskunta",
+      "license": "CC BY 3.0 NO"
     },
     {
       "filename": "4.jpg",
-      "source": "www.example.com",
-      "license": "CC-BY"
+      "source": "https://commons.wikimedia.org/wiki/File:Eduskunta_istuntosali_Session_Hall_of_Parliament_House_%28Finland%29_03.jpg",
+      "license": "CC BY-SA 4.0"
     }
   ],
   "emmanuel macron": [
     {
       "filename": "1.jpeg",
-      "source": "www.example.com",
-      "license": "CC-BY"
+      "source": "https://commons.wikimedia.org/wiki/File:Members_debated_the_French_Presidency%E2%80%99s_priorities_with_Emmanuel_Macron_-_51830991330_%28cropped%29.jpg",
+      "license": "CC BY 2.0"
     },
     {
       "filename": "2.jpeg",
-      "source": "www.example.com",
-      "license": "CC-BY"
+      "source": "https://nara.getarchive.net/media/president-emmanuel-macron-the-president-of-france-e4cf32",
+      "license": "PDM 1.0"
     },
     {
       "filename": "3.jpg",
-      "source": "www.example.com",
-      "license": "CC-BY"
+      "source": "https://www.flickr.com/photos/faces-of-the-world/51419852660",
+      "license": "CC BY 2.0"
     }
   ],
   "euroopan keskuspankki": [
     {
       "filename": "1.jpg",
-      "source": "www.example.com",
-      "license": "CC-BY"
-    },
-    {
-      "filename": "2.jpeg",
-      "source": "www.example.com",
-      "license": "CC-BY"
+      "source": "https://commons.wikimedia.org/wiki/File:European_Central_Bank_-_building_under_construction_-_Frankfurt_-_Germany_-_14.jpg",
+      "license": "CC BY-SA 3.0"
     }
   ],
   "euroopan unioni": [
     {
       "filename": "1.jpg",
-      "source": "www.example.com",
-      "license": "CC-BY"
+      "source": "https://jonathanrileywriter.medium.com/european-union-and-why-it-needs-to-change-fee2d688a7eb",
+      "license": "CC0 1.0"
     },
     {
       "filename": "2.jpg",
-      "source": "www.example.com",
-      "license": "CC-BY"
+      "source": "https://www.article19.org/resources/european-commissioners-designate-hearings-insights/",
+      "license": "CC0 1.0"
     }
   ],
-  "eurooppa": [
-    {
-      "filename": "1.png",
-      "source": "www.example.com",
-      "license": "CC-BY"
-    }
-  ],
+
   "helsinki": [
     {
       "filename": "1.jpg",
-      "source": "www.example.com",
-      "license": "CC-BY"
+      "source": "https://www.flickr.com/photos/pedrosz/49694131566",
+      "license": "CC BY-SA 2.0"
     }
   ],
   "hsl": [
     {
       "filename": "1.jpg",
-      "source": "www.example.com",
-      "license": "CC-BY"
+      "source": "https://www.flickr.com/photos/86084595@N08/37867134336",
+      "license": "CC BY-NC-ND 2.0"
     }
   ],
   "itämeri": [
     {
       "filename": "1.jpg",
-      "source": "www.example.com",
-      "license": "CC-BY"
+      "source": "https://commons.wikimedia.org/wiki/File:Baltic_Sea,_Sweden.jpg",
+      "license": "CC BY-SA 4.0"
     }
   ],
   "jussi halla-aho": [
     {
       "filename": "1.jpg",
-      "source": "www.example.com",
-      "license": "CC-BY"
+      "source": "https://www.flickr.com/photos/oireachtas/53219212987",
+      "license": "CC BY 2.0"
     },
     {
       "filename": "2.jpeg",
-      "source": "www.example.com",
-      "license": "CC-BY"
+      "source": "https://www.flickr.com/photos/54097310@N08/53219213177",
+      "license": "CC BY 2.0"
     }
   ],
   "leipomo": [
     {
       "filename": "1.jpeg",
-      "source": "www.example.com",
-      "license": "CC-BY"
+      "source": "https://pxhere.com/en/photo/1416594",
+      "license": "CC0 1.0"
     }
   ],
   "mark rutte": [
     {
       "filename": "1.jpg",
-      "source": "www.example.com",
-      "license": "CC-BY"
+      "source": "https://commons.wikimedia.org/wiki/File:Volodymyr_Zelensky_met_with_Dutch_PM_Mark_Rutte_in_occasion_of_possible_Russian_invasion_%2826%29.jpg",
+      "license": "CC BY 4.0"
     }
   ],
   "metsä": [
     {
       "filename": "1.jpg",
-      "source": "www.example.com",
-      "license": "CC-BY"
+      "source": "https://www.flickr.com/photos/ansik/1374647348",
+      "license": "CC BY 2.0"
     }
   ],
   "nato": [
     {
       "filename": "1.jpg",
-      "source": "www.example.com",
-      "license": "CC-BY"
+      "source": "https://www.flickr.com/photos/finnishgovernment/52792851125",
+      "license": "CC BY 2.0"
     },
     {
       "filename": "2.jpg",
-      "source": "www.example.com",
-      "license": "CC-BY"
+      "source": "https://www.flickr.com/photos/defenceimages/14386519750",
+      "license": "CC BY-NC 2.0"
     }
   ],
   "oikeudenkäynti": [
     {
       "filename": "1.jpeg",
-      "source": "www.example.com",
-      "license": "CC-BY"
+      "source": "https://picryl.com/media/hammer-court-judge-f3fd65",
+      "license": "PDM 1.0"
     },
     {
       "filename": "2.jpeg",
-      "source": "www.example.com",
-      "license": "CC-BY"
+      "source": "https://www.pexels.com/photo/a-golden-balance-scale-beside-a-laptop-6077797/",
+      "license": "CC0"
     }
   ],
   "olaf scholz": [
     {
       "filename": "1.jpg",
-      "source": "www.example.com",
-      "license": "CC-BY"
+      "source": "https://www.flickr.com/photos/oscepa/42369342835",
+      "license": "CC BY-SA 2.0"
     },
     {
       "filename": "2.jpg",
-      "source": "www.example.com",
-      "license": "CC-BY"
+      "source": "https://www.flickr.com/photos/worldeconomicforum/52100818760",
+      "license": "CC BY-NC-SA 2.0"
     }
   ],
   "osakemarkkinat": [
     {
       "filename": "1.jpeg",
-      "source": "www.example.com",
-      "license": "CC-BY"
+      "source": "https://cyprus-mail.com/2024/11/29/spooked-by-trade-tensions-global-stocks-to-deliver-tempered-gains-in-2025",
+      "license": "CC BY-ND 4.0"
     },
     {
       "filename": "2.jpeg",
-      "source": "www.example.com",
-      "license": "CC-BY"
+      "source": "https://lhsmagpie.com/8741/features/what-is-the-stock-market/",
+      "license": "CC BY-ND 4.0"
     }
   ],
   "pentagon": [
     {
       "filename": "1.jpg",
-      "source": "www.example.com",
-      "license": "CC-BY"
+      "source": "https://www.flickr.com/photos/wiyre/44579666601",
+      "license": "CC BY 2.0"
     }
   ],
   "petteri orpo": [
     {
-      "filename": "1.jpeg",
-      "source": "www.example.com",
-      "license": "CC-BY"
+      "filename": "1.jpg",
+      "source": "https://commons.wikimedia.org/wiki/File:Petteri-Orpo-O45A3040.jpg",
+      "license": "CC BY 4.0"
     },
     {
-      "filename": "2.jpeg",
-      "source": "www.example.com",
-      "license": "CC-BY"
+      "filename": "2.jpg",
+      "source": "https://commons.wikimedia.org/wiki/File:Petteri-Orpo-01.jpg",
+      "license": "CC BY 4.0"
     }
   ],
   "ranska": [
     {
       "filename": "1.jpg",
-      "source": "www.example.com",
-      "license": "CC-BY"
+      "source": "https://www.goodfon.com/city/wallpaper-paris-france-la-tour-eiffel-321.html",
+      "license": "CC BY-NC 4.0"
     },
     {
       "filename": "2.jpg",
-      "source": "www.example.com",
-      "license": "CC-BY"
+      "source": "https://freerangestock.com/photos/87272/red-white-and-blue-painting-of-the-flag-of-france.html",
+      "license": "CC0 1.0"
     }
   ],
   "riikka purra": [
     {
       "filename": "1.jpeg",
-      "source": "www.example.com",
-      "license": "CC-BY"
+      "source": "https://commons.m.wikimedia.org/wiki/File:Riikka_Purra_-_Hallituksen_tiedotustilaisuus_31.08.2023_-_53154772884.jpg",
+      "license": "CC BY 2.0"
     }
   ],
   "saksa": [
     {
       "filename": "1.png",
-      "source": "www.example.com",
-      "license": "CC-BY"
+      "source": "https://picryl.com/media/germany-german-germany-flag-architecture-buildings-cc32f4",
+      "license": "PDM 1.0"
     },
     {
       "filename": "2.jpg",
-      "source": "www.example.com",
-      "license": "CC-BY"
+      "source": "https://itoldya420.getarchive.net/media/federal-republic-of-germany-germany-schland-f4aabf",
+      "license": "CC0 1.0"
     }
   ],
   "suomen hallitus": [
     {
-      "filename": "1.jpeg",
-      "source": "www.example.com",
-      "license": "CC-BY"
+      "filename": "1.png",
+      "source": "https://www.flickr.com/photos/finnishgovernment/53198097769",
+      "license": "CC BY 2.0"
     }
   ],
   "sähkö": [
     {
       "filename": "1.jpg",
-      "source": "www.example.com",
-      "license": "CC-BY"
-    },
-    {
-      "filename": "2.jpg",
-      "source": "www.example.com",
-      "license": "CC-BY"
+      "source": "https://picryl.com/media/france-rhone-river-industry-craft-fda029",
+      "license": "PDM 1.0"
     },
     {
       "filename": "3.jpg",
-      "source": "www.example.com",
-      "license": "CC-BY"
+      "source": "https://www.stockvault.net/photo/186953/electricity",
+      "license": "CC0 1.0"
     },
     {
       "filename": "4.jpg",
-      "source": "www.example.com",
-      "license": "CC-BY"
+      "source": "https://pxhere.com/fi/photo/913914",
+      "license": "CC0 1.0"
     }
   ],
   "talous": [
     {
       "filename": "1.jpeg",
-      "source": "www.example.com",
-      "license": "CC-BY"
+      "source": "https://www.pickpik.com/money-bank-note-euro-banknote-paper-money-bill-148500",
+      "license": "CC0 1.0"
     },
     {
       "filename": "2.jpeg",
-      "source": "www.example.com",
-      "license": "CC-BY"
+      "source": "https://pxhere.com/fi/photo/926983",
+      "license": "CC0 1.0"
     },
     {
       "filename": "3.jpg",
-      "source": "www.example.com",
-      "license": "CC-BY"
+      "source": "https://theberkshireedge.com/capital-ideas-how-much-will-the-stock-market-gain-or-lose-in-2025/",
+      "license": "CC BY-ND 4.0"
     },
     {
       "filename": "4.jpg",
-      "source": "www.example.com",
-      "license": "CC-BY"
+      "source": "https://commons.wikimedia.org/wiki/File:10_euro_banknotes_and_10_euro_cent_coins_20180207.jpg",
+      "license": "CC BY-SA 4.0"
     }
   ],
   "terveydenhuolto": [
     {
       "filename": "1.png",
-      "source": "www.example.com",
-      "license": "CC-BY"
+      "source": "https://pxhere.com/en/photo/917209",
+      "license": "CC0 1.0"
     },
     {
       "filename": "3.jpg",
-      "source": "www.example.com",
-      "license": "CC-BY"
+      "source": "https://picryl.com/media/doctor-medical-medicine-health-medical-9ce6bf",
+      "license": "PDM 1.0"
     },
     {
       "filename": "4.jpg",
-      "source": "www.example.com",
-      "license": "CC-BY"
+      "source": "https://www.flickr.com/photos/elentir/4504031924",
+      "license": "CC BY-SA 2.0"
     }
   ],
   "tuulivoima": [
     {
       "filename": "1.jpg",
-      "source": "www.example.com",
-      "license": "CC-BY"
+      "source": "https://www.stockvault.net/photo/201730/wind-turbines",
+      "license": "CC0 1.0"
     }
   ],
   "ukraina": [
     {
       "filename": "1.jpg",
-      "source": "www.example.com",
-      "license": "CC-BY"
+      "source": "https://www.cid.org.nz/connect/news/crisis-unfolds-following-attack-on-ukraine-dam/",
+      "license": "CC0 1.0"
     }
   ],
   "venäjä": [
     {
       "filename": "1.jpeg",
-      "source": "www.example.com",
-      "license": "CC-BY"
+      "source": "https://www.pexels.com/photo/kremlin-in-moscow-russia-18394876/",
+      "license": "CC0"
+    },
+    {
+      "filename": "2.jpeg",
+      "source": "https://www.sipa.columbia.edu/news/russia-confirms-yevgeny-prigozhin-died-plane-crash",
+      "license": "CC0 1.0"
     }
   ],
   "vladimir putin": [
     {
       "filename": "1.jpeg",
-      "source": "www.example.com",
-      "license": "CC-BY"
+      "source": "https://www.freemalaysiatoday.com/category/business/2025/02/07/putin-hails-russias-strong-2024-economic-growth/",
+      "license": "CC BY 4.0"
     }
   ],
   "volodymyr zelenskyi": [
     {
       "filename": "1.jpeg",
-      "source": "www.example.com",
-      "license": "CC-BY"
+      "source": "https://www.freemalaysiatoday.com/category/world/2025/03/24/zelensky-says-must-push-putin-to-stop-strikes-halt-invasion/",
+      "license": "CC BY 4.0"
     },
     {
       "filename": "2.jpeg",
-      "source": "www.example.com",
-      "license": "CC-BY"
+      "source": "https://www.freemalaysiatoday.com/category/world/2025/02/12/zelensky-offers-land-swaps-as-russia-courts-trump-with-prisoner-release/",
+      "license": "CC BY 4.0"
     },
     {
       "filename": "3.jpg",
-      "source": "www.example.com",
-      "license": "CC-BY"
+      "source": "https://www.agenzianova.com/en/news/attacco-russo-nella-notte-zelensky-putin-rifiuta-il-cessate-il-fuoco/",
+      "license": "CC0 1.0"
     }
   ],
   "yhdysvallat": [
     {
       "filename": "1.jpeg",
-      "source": "www.example.com",
-      "license": "CC-BY"
+      "source": "https://pxhere.com/fi/photo/1520929",
+      "license": "CC0 1.0"
     },
     {
       "filename": "2.jpeg",
-      "source": "www.example.com",
-      "license": "CC-BY"
+      "source": "https://pxhere.com/fi/photo/1614066",
+      "license": "CC0 1.0"
     },
     {
       "filename": "3.jpeg",
-      "source": "www.example.com",
-      "license": "CC-BY"
+      "source": "https://pxhere.com/fi/photo/1499871",
+      "license": "CC0 1.0"
     }
   ]
 };
 
-
-
-
-
 const baseImageURL = "https://juicy-monkey.github.io/uutisydin_node";
-// const baseImageURL = "file:///Users/jusa/workspace/Juicy_Monkey/projects/uutisydin/uutisydin_node/public";
 
 function displayImages(imageData) {
   const container = document.getElementById("image-list");
@@ -466,7 +450,7 @@ function displayImages(imageData) {
       link.rel = "noopener noreferrer";
 
       const license = document.createElement("p");
-      license.textContent = `License: ${image.license}`;
+      license.textContent = image.license;
 
       imgDiv.appendChild(img);
       imgDiv.appendChild(link);
